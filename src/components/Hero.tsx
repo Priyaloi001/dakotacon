a27f500a058175645ed69a6fc817851d421e8a47
Garima8284 <developerpcl001@gmail.com>
2025-07-23 12:36:19 +0530

    home register button link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 const Hero = () => {
     return (
         <div style={{ backgroundImage: "url('/assets/images/banner1.jpg')" }} className="h-[600px] xl:h-[700px] bg-cover bg-no-repeat px-5 xl:px-0">
@@ -9,7 +10,7 @@ const Hero = () => {
                     <p className="font-primary font-base leading">
                         Join us for three full days of talks and trainings from the finest peeps in the security world! Come enjoy the benefits of a small conference where you won't get lost in the crowd and you get time to interact directly with the speakers and your security peers. Attending the talks on Friday is FREE, so you have no excuse!
                     </p>
-                    <a href="#" className="font-primary capitalize text-white  border-1 border-white py-3 px-10 rounded-md text-sm md:text-base hover:bg-white hover:text-black hover:border-black duration-200">register</a>
+                     <NavLink to="/register" className="font-primary capitalize text-white  border-1 border-white py-3 px-10 rounded-md text-sm md:text-base hover:bg-white hover:text-black hover:border-black duration-200">register</NavLink>
                 </div>
             </div>
         </div>
